ecbc00c6a20ffe31fc2ce1894bd33b9e530b8207
jmhouf <houjimin_91@163.com>
2022-01-14 01:52:57 +0800

    day1

--- src/huawei/online/test001/LastWordLength.java
+++ src/huawei/online/test001/LastWordLength.java
@@ -3,8 +3,26 @@ package test001;
 import java.util.Scanner;
 
 public class LastWordLength {
+    /**
+     * HJ1 字符串最后一个单词的长度
+     * 描述
+     * 计算字符串最后一个单词的长度，单词以空格隔开，字符串长度小于5000。（注：字符串末尾不以空格为结尾）
+     * 输入描述：
+     * 输入一行，代表要计算的字符串，非空，长度小于5000。
+     *
+     * 输出描述：
+     * 输出一个整数，表示输入字符串最后一个单词的长度。
+     * 示例1
+     * 输入：hello nowcoder
+     * 输出：8
+     * 说明： 最后一个单词为nowcoder，长度为8
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        /* 直接拆分输入行，获取最后一个子串，再去长度属性
+         */
         String[] words = sc.nextLine().split(" ");
         System.out.println(words[words.length - 1].length());
     }
--- src/huawei/online/test002/FindTimes.java
+++ src/huawei/online/test002/FindTimes.java
@@ -3,6 +3,28 @@ package test002;
 import java.util.Scanner;
 
 public class FindTimes {
+    /**
+     * HJ2 计算某字符出现次数
+     * 描述
+     * 写出一个程序，接受一个由字母、数字和空格组成的字符串，和一个字符，然后输出输入字符串中该字符的出现次数。（不区分大小写字母）
+     *
+     * 数据范围：
+     * 输入描述：
+     * 第一行输入一个由字母和数字以及空格组成的字符串，第二行输入一个字符。
+     *
+     * 输出描述：
+     * 输出输入字符串中含有该字符的个数。（不区分大小写字母）
+     *
+     * 示例1
+     * 输入：
+     * ABCabc
+     * A
+     *
+     * 输出：
+     * 2
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String strs = sc.nextLine().toLowerCase();
